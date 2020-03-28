089888db619adcf9831cada6820bae82a15dcee3
Brumus14 <luke.barkess@icloud.com>
2020-03-28 17:54:31 +0000

    Added the Dexplore files

---
+++ Dexplore/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathMenu : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ToggleDeathMenu()
+    {
+        gameObject.SetActive(true);
+    }
+}
